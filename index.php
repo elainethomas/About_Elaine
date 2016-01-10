76d4e94bfa9b226759f071829920aadb75095c3d
Elaine Thomas <enajera2@cnm.edu>
2016-01-10 11:49:55 -0700

    Adding an index page to About_Elaine

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>
+			This is the title
+		</title>
+		<link href="/style1.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<h1>This is a webpage about Elaine</h1>
+		<p>
+			These are some words and things here is something <i>italicized.</i>
+		</p>
+
+		<h2>This is an ordered list of things</h2>
+		<ol>
+			<li>This is an item</li>
+			<li>This is the next item</li>
+			<li>This is the last item</li>
+		</ol>
+
+		<p>
+			This is a statement about stuff.
+		</p>
+	</body>
+</html>
